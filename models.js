bd85dad1adaecde8c98a4a49ccc3ac602b4c707e
Nicolas Cardenas <nico.cardenas260494@gmail.com>
2024-09-14 13:25:08 -0300

    modificacion de models

--- models.js
+++ models.js
@@ -1,14 +1,17 @@
 import { existsSync, readFileSync, writeFileSync } from "node:fs";
 import { randomUUID } from "node:crypto";
-// Averiguar que importar de NODE para realizar el hash del pass
-// Averiguar como "activar" la lectura de las variables de entorno del archivo .env (dotenv)
 import { handleError } from "./utils/handleError.js";
 
-// 1° recuperar variables de entorno
-
-// 2° Declarar los metodos
+const PATH_FILE = ".data/users.json";
 
 const getUsers = () => {
+  const existsFile = existsSync(PATH_FILE);
+  if (!existsFile) {
+    writeFileSync(PATH_FILE, JSON.stringify([]));
+    return [];
+  }
+  return JSON.parse(readFileSync(PATH_FILE));
+
   try {
   } catch (error) {
     // const objError = handleError()
@@ -17,8 +20,8 @@ const getUsers = () => {
 };
 
 const getUserById = (id) => {
-  try {
-  } catch (error) {}
+  // try {
+  //} catch (error) {}
 };
 
 // addUser recibe un objeto con toda la data para el nuevo usuario
@@ -28,19 +31,74 @@ const getUserById = (id) => {
 // valida que el email sea un string y que no se repita
 // hashea la contraseña antes de registrar al usuario
 const addUser = (userData) => {
-  try {
-  } catch (error) {}
+  const { nombre, apellido, email, contraseña } = userData;
+
+  const users = getUsers();
+
+  const existUser = users.find((userData) => userData.nombre === nombre);
+
+  if (existUser) {
+    return "usuario ya existente";
+  }
+
+  const newUser = {
+    id: randomUUID(),
+    nombre,
+    apellido,
+    email: email,
+    contraseña,
+  };
+
+  users.push(newUser);
+
+  writeFileSync(PATH_FILE, JSON.stringify(users));
+
+  return "usuario creado";
+
+  //try {
+  //} catch (error) {}
 };
 
 // todos los datos del usuario seleccionado se podrían modificar menos el ID
 // si se modifica la pass debería ser nuevamente hasheada
 // si se modifica el email, validar que este no exista
-const updateUser = (userData) => {
-  try {
-  } catch (error) {}
+const updateUser = (id, userData) => {
+  const { nombre, apellido, email, contraseña } = userData;
+
+  const users = getUsers();
+
+  const existUser = users.find((userData) => userData.id === id);
+
+  if (!existUser) {
+    return "usuario no existente";
+  }
+
+  if (nombre) existUser.nombre = nombre;
+  if (apellido) existUser.apellido = apellido;
+  if (email) existUser.email = email;
+  if (contraseña) existUser.contraseña = contraseña;
+
+  writeFileSync(PATH_FILE, JSON.stringify(users));
+  return existUser;
+  // try {
+  // } catch (error) {}
 };
 
 const deleteUser = (id) => {
+  const users = getUsers();
+
+  const existsUser = users.find((userData) => userData.id === id);
+
+  if (!existsUser) {
+    return "Not found user";
+  }
+
+  const newUser = users.filter((user) => user.id !== id);
+
+  writeFileSync(PATH_FILE, JSON.stringify(newUser));
+
+  return newUser;
+
   try {
   } catch (error) {}
 };
